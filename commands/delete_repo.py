4e0dd060b7b6f7797670b803989b92a64fd41b38
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-06 17:42:41 +0200

    removed a debug line

--- commands/delete_repo.py
+++ commands/delete_repo.py
@@ -3,8 +3,6 @@ import requests, sys, os
 def delete_repo(api_url, user, token, repo):
     login = requests.delete(api_url + 'repos/' + user + '/' + repo, auth=(user,token))
 
-    print(login.status_code)
-
     if login.status_code == 204:
         print('\033[32m[OK]\033[0m Repo deleted')
     else:
