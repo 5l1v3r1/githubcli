0a2d0ae82eeade3dc56de14e7e4246900454a6b8
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-06 17:26:15 +0200

    minor bugfix

--- commands/delete_repo.py
+++ commands/delete_repo.py
@@ -3,7 +3,9 @@ import requests, sys, os
 def delete_repo(api_url, user, token, repo):
     login = requests.delete(api_url + 'repos/' + user + '/' + repo, auth=(user,token))
 
-    if login.status_code == 201:
-        print('\033[31m[%s]\033[0m Repo does not exist' % login.status_code)
-    else:
+    print(login.status_code)
+
+    if login.status_code == 204:
         print('\033[32m[OK]\033[0m Repo deleted')
+    else:
+        print('\033[31m[%s]\033[0m Cannot delete repo' % login.status_code)
