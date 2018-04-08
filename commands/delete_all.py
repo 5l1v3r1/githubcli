a27c6f5eac1ef7f2c14cac1ca9190e0353b8099b
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-08 23:47:09 +0200

    Updated for a even better FML experience

--- commands/delete_all.py
+++ commands/delete_all.py
@@ -7,8 +7,10 @@ def delete_all(api_url, user, token):
     login = requests.get(api_url + 'user/repos', auth=(user, token))
 
     repos = login.json()
+    c = 0
 
     for url in repos:
-        url = url["html_url"]
-        repo = url.split('/')[-1]
+        repo = url["html_url"].split('/')[-1]
         delete = requests.delete(api_url + 'repos/' + user + '/' + repo, auth=(user,token))
+        c+=1
+        print('\033[31mDeleted %s [%i/%i]\033[0m' % (repo, int(c), len(repos)))
