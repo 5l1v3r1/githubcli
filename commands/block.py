ed78b1901380529ebb222611f71ccd148da3d65c
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-11 18:35:30 +0200

    fixed list blocked users, but api denies its usage

--- commands/block.py
+++ commands/block.py
@@ -22,14 +22,20 @@ def unblock(api_url, user, token, username):
         print('\033[31m[%s]\033[0m Cannot unblock' % login.status_code)
 
 def blocks(api_url, user, token):
+
     login = requests.get(api_url + 'user/blocks', auth=(user,token))
 
+    data = login.json()
     c = 0
 
     header = "Username".ljust(25), "Profile".ljust(35), "Site Admin"
     print('\033[34m{0[0]} {0[1]} {0[2]}\033[0m'.format(header))
 
-    for blocked in login.json():
-        print('%s %s %s' % (blocked["login"].ljust(22), blocked["html_url"].ljust(38), blocked["site_admin"]))
+    for i in range(len(data)):
+        print('%s %s %s' % (data[i]["login"].ljust(22), data[i]["html_url"].ljust(38), data[i]["site_admin"]))
         c+=1
     print('\033[32mFound %i blocked users\033[0m' % int(c))
+
+    #for blocked in login.json():
+    #    print('%s %s %s' % (blocked["login"].ljust(22), blocked["html_url"].ljust(38), blocked["site_admin"]))
+    #    c+=1
