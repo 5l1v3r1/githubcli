b597213a39e5c940f84198d46c23800d1f364f59
Leon V <33411715+leonv024@users.noreply.github.com>
2018-04-13 10:53:06 +0200

    adjusted ljust()
    some filenames are bigger than expected.

--- commands/get_files.py
+++ commands/get_files.py
@@ -4,18 +4,19 @@ def get_files(api_url, user, token, username, repo):
     login = requests.get(api_url + 'repos/' + username + '/' + repo + '/contents', auth=(user, token))
 
     data = login.json()
-    header = "Path".ljust(40), "Type".ljust(20), "Size"
+    header = "Path".ljust(70), "Type".ljust(20), "Size"
     print('\033[37m{0[0]} {0[1]} {0[2]}\033[0m'.format(header))
 
     for i in range(len(data)):
         path = data[i]["path"]
         size = data[i]["size"]
         _type = data[i]["type"]
-        print('%s %s %s' % (path.ljust(40), _type.ljust(20), size))
+        print('%s %s %s' % (path.ljust(70), _type.ljust(20), size))
 
         if _type == 'dir':
             get_dir_files(api_url, user, token, username, repo, path)
 
+# Search all levels
 def get_dir_files(api_url, user, token, username, repo, path):
     login = requests.get(api_url + 'repos/' + username + '/' + repo + '/contents/' + path, auth=(user, token))
     data = login.json()
@@ -23,6 +24,6 @@ def get_dir_files(api_url, user, token, username, repo, path):
         path = data[i]["path"]
         size = data[i]["size"]
         _type = data[i]["type"]
-        print('%s %s %s' % (path.ljust(40), _type.ljust(20), size))
+        print('%s %s %s' % (path.ljust(70), _type.ljust(20), size))
         if _type == "dir":
             return get_dir_files(api_url, user, token, username, repo, path) # Search all levels
